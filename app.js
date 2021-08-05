d9aef22356a2b7af05ac2cdd70c251e000874b3d
LamQuoc24 <82473096+LamQuoc24@users.noreply.github.com>
2021-08-05 17:33:45 -0400

    link fade animation and burger x animation

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
+//toggle nav
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
+ //Opacity Links       
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+             link.style.animation = `navLinkFade 0.5s ease forwards ${index/5 + .8}s`;};
+            });
+
+//X Burger
+        burger.classList.toggle('andrea');
 });
 }
 
